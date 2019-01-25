8e0ed0666bce988019757e854aac443dcd754e55
xbreizh <xbreizh@yahoo.co.nz>
2019-01-25 18:30:19 +0100

    moved items to config file

---
+++ troparo-business/src/main/java/org/troparo/business/impl/EmailManager.java
@@ -0,0 +1,132 @@
+package org.troparo.business.impl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Properties;
+
+@Component
+@Configuration
+@EnableScheduling
+@PropertySource("classpath:keypassword.properties")
+public class EmailManager {
+
+    @Value("${fromEmail}")
+    private String mailFrom;
+    @Value("${fileLocation}")
+    private String fileLocation;
+    @Value("${subject}")
+    private String subject;
+    @Value("${body}")
+    private String body;
+
+    @Scheduled(cron = "*/20 * * * * *")
+    public void sendMail() {
+        final String username = "xavier.lamourec@gmail.com";
+
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        try {
+                            return new PasswordAuthentication(username, getPassword());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(mailFrom));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("dontkillewok@gmail.com"));
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static final String AES = "AES";
+
+    private String byteArrayToHexString(byte[] b) {
+        StringBuffer sb = new StringBuffer(b.length * 2);
+        for (int i = 0; i < b.length; i++) {
+            int v = b[i] & 0xff;
+            if (v < 16) {
+                sb.append('0');
+            }
+            sb.append(Integer.toHexString(v));
+        }
+        return sb.toString().toUpperCase();
+    }
+
+    private byte[] hexStringToByteArray(String s) {
+        byte[] b = new byte[s.length() / 2];
+        for (int i = 0; i < b.length; i++) {
+            int index = i * 2;
+            int v = Integer.parseInt(s.substring(index, index + 2), 16);
+            b[i] = (byte) v;
+        }
+        return b;
+    }
+
+  /*  public static void main(String args[]) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, FileNotFoundException, IOException {
+
+        getPassword();
+
+    }*/
+
+    private String getPassword() throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        String tempkey = "";
+        String password = "";
+        Properties prop = new Properties();
+        InputStream input = null;
+        input = new FileInputStream(fileLocation);
+        // load a properties file
+        prop.load(input);
+        tempkey = prop.getProperty("Key");
+        password = prop.getProperty("Encrypted_Password");
+
+        byte[] bytekey = hexStringToByteArray(tempkey);
+        SecretKeySpec sks = new SecretKeySpec(bytekey, AES);
+        Cipher cipher = Cipher.getInstance(AES);
+        cipher.init(Cipher.DECRYPT_MODE, sks);
+        byte[] decrypted = cipher.doFinal(hexStringToByteArray(password));
+        String OriginalPassword = new String(decrypted);
+
+        return OriginalPassword;
+    }
+}
